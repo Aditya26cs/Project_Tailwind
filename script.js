6449846201e214cb94845abe2396f32986d09528
Aditya Maheshwari <aditya.2226cs1121@kiet.edu>
2024-05-16 20:23:28 +0530

    done

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const line3 = document.getElementById("line3")
 function handlemenu(){
       line3.classList.toggle("hidden")
 }
+
+const dtElements = document.querySelectorAll('dt');
+
+dtElements.forEach(element => {
+      element.addEventListener("click" , () => {
+            const ddID = element.getAttribute('aria-controls');
+            const ddElement = document.getElementById(ddID);
+            const ddArrowIcon = element.querySelectorAll('i')[0];
+
+            ddElement.classList.toggle('hidden');
+            ddArrowIcon.classList.toggle('-rotate-180');
+      })
+})
